dc7d9128ed3a83571faae0ffc41c9e3800935c8e
SongWei <xdudetermined@live.com>
2014-04-08 15:15:36 +0800

    base demo

---
+++ MatPassword/GridVM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MatPassword
+{
+    class GridVM : DependencyObject
+    {
+        public Border Item
+        {
+            get { return (Border)GetValue(ItemProperty); }
+            set { SetValue(ItemProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Item.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemProperty =
+            DependencyProperty.Register("Item", typeof(Border), typeof(GridVM), new PropertyMetadata(null));
+        
+        public Brush Back
+        {
+            get { return (Brush)GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Background.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BackgroundProperty =
+            DependencyProperty.Register("Background", typeof(Brush), typeof(GridVM), new PropertyMetadata(null));
+
+        public System.Windows.Media.ImageSource Img
+        {
+            get { return (System.Windows.Media.ImageSource)GetValue(ImgProperty); }
+            set { SetValue(ImgProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Img.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImgProperty =
+            DependencyProperty.Register("Img", typeof(System.Windows.Media.ImageSource), typeof(GridVM), new PropertyMetadata(null));
+    }
+}
---
+++ MatPassword/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.Dynamic;
+using System.Collections.ObjectModel;
+
+namespace MatPassword
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            var mat = new MatMaker();
+            lstMat.ItemsSource = mat.VMS;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var mat = new MatMaker();
+            lstMat.ItemsSource = mat.VMS;
+        }
+    }
+}
